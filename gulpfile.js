a0747df9d855cc16310920cc4ae71b3b0ff93fbf
Ibiyemi Abiodun <ibiyemi@intulon.com>
2020-05-17 17:12:57 -0400

    latest changes

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,25 @@ sass.compiler = require('node-sass');
 const tsProject = ts.createProject('tsconfig.json');
 
 function style() {
-    return src('src/**/*.scss')
-        .pipe(sass())
-        .pipe(dest('dist'));
+  return src('src/**/*.scss')
+    .pipe(sass())
+    .pipe(dest('dist'));
 }
 
 function script() {
-    return src('src/**/*.ts')
-        .pipe(tsProject())
-        .pipe(dest('dist'));
+  return src('src/**/*.ts')
+    .pipe(tsProject())
+    .pipe(dest('dist'));
 }
 
 function markup() {
-    return src('src/**/*.html')
-        .pipe(dest('dist'));
+  return src('src/**/*.html')
+    .pipe(dest('dist'));
 }
 
 exports.default = parallel(style, script, markup);
 exports.watch = () => {
-    watch("src/**/*.scss", { ignoreInitial: false }, style);
-    watch("src/**/*.ts", { ignoreInitial: false }, script);
-    watch("src/**/*.html", { ignoreInitial: false }, markup);
+  watch('src/**/*.scss', { ignoreInitial: false }, style);
+  watch('src/**/*.ts', { ignoreInitial: false }, script);
+  watch('src/**/*.html', { ignoreInitial: false }, markup);
 };
